a55a260137b3accb640c75e0b1698cf616364a1c
curtismchale <curtis@curtismchale.ca>
2012-06-05 10:20:11 -0700

    missing ,

--- coupons-for-gravityforms.php
+++ coupons-for-gravityforms.php
@@ -29,7 +29,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/curtismchale/Coupons-for-Gravity-Forms/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms', // the github url of your github repo
         'zip_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms/zipball/master', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.4', // which version of WordPress is your plugin tested up to?
         'readme' => 'readme.txt' // which file to use as the readme for the version number
