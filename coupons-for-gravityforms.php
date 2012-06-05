7bc6ba6032e9c6642e8917a4e4f1436df84050f0
curtismchale <curtis@curtismchale.ca>
2012-06-05 10:30:13 -0700

    working out my issues with the updater

--- coupons-for-gravityforms.php
+++ coupons-for-gravityforms.php
@@ -15,27 +15,33 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * @todo update the 'expire/published' notice in the admin so that the language reads better
  */
 
-/* includes the update from Github code */
-require_once( plugin_dir_path( __FILE__ ) . '/updater.php' );
-
 /* calls the new CPT  that will make up the coupons */
 require_once( plugin_dir_path( __FILE__ ) . '/create-coupon-cpt.php' );
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'coupons-for-gravityforms', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/curtismchale/Coupons-for-Gravity-Forms', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/curtismchale/Coupons-for-Gravity-Forms/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms', // the github url of your github repo
-        'zip_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms/zipball/master', // the zip url of the github repo
-        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.4', // which version of WordPress is your plugin tested up to?
-        'readme' => 'readme.txt' // which file to use as the readme for the version number
-    );
-    new WPGitHubUpdater($config);
+function sfn_gfcoupon_github_updater(){
+
+  /* includes the update from Github code */
+  require_once( plugin_dir_path( __FILE__ ) . '/updater.php' );
+
+  define('WP_GITHUB_FORCE_UPDATE', true);
+
+  if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+      $config = array(
+          'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+          'proper_folder_name' => 'coupons-for-gravityforms', // this is the name of the folder your plugin lives in
+          'api_url' => 'https://api.github.com/repos/curtismchale/Coupons-for-Gravity-Forms', // the github API url of your github repo
+          'raw_url' => 'https://raw.github.com/curtismchale/Coupons-for-Gravity-Forms/master', // the github raw url of your github repo
+          'github_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms', // the github url of your github repo
+          'zip_url' => 'https://github.com/curtismchale/Coupons-for-Gravity-Forms/zipball/master', // the zip url of the github repo
+          'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+          'requires' => '3.0', // which version of WordPress does your plugin require?
+          'tested' => '3.4', // which version of WordPress is your plugin tested up to?
+          'readme' => 'readme.txt' // which file to use as the readme for the version number
+      );
+      new WPGitHubUpdater($config);
+  }
 }
+add_action( 'init', 'sfn_gfcoupon_github_updater' );
 
 /*
  * Produces print_r inside <pre> limited to development users
